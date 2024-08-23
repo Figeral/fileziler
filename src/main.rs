65213b1c8105ac8037f897d6f20cd91b86134d4c
Figeral <mouliofitzgerard@gmail.com>
2024-08-23 14:30:00 +0100

    implementation of modulation

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use clap::{Parser, ValueEnum};
+
 use fileziler::Args;
 
 fn main() {
     let args = Args::parse();
     start(&args);
 }
+
 fn start(args: &Args) {
     println!("{:?}", args.opt);
 }
